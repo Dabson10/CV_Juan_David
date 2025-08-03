4950fb705b6100fb16c6ef4991d87f120de04c94
juan david <dabson753@gmail.com>
2025-08-03 00:35:19 -0600

    Modificacion significativo en el estilo, solo esta la portada y aun faltan los media

--- scritp.js
+++ scritp.js
@@ -1,64 +1,54 @@
-// Esta seccion son las variables de los botones para activar cada seccion
+document.addEventListener('DOMContentLoaded', () =>{
+    //Este boton hara aparecer y desaparecer la sección de los datos personales
+    //y la sección de proyectos.
+    const btnAparecer = document.getElementById('btnPie')
+    const svgFlecha = document.querySelectorAll('.svgAbajo')
+    const contCuerpoPort = document.querySelector('.cuerpoHeader')
+    const contHeader = document.getElementById('header')
+    const txtCambiar = document.getElementById('textoCambio')
+    let btnPresionado = false
 
-const btnJava = document.getElementById('btnJava')
-const btnC = document.getElementById('btnC')
-const btnHtml = document.getElementById('btnHtml')
 
-// Variable de los contenedores de cada seccion
-const contJava = document.getElementById("contJava")
-const contC = document.getElementById("contC")
-const contHtml = document.getElementById("contHtml")
+    if(btnAparecer && svgFlecha.length > 0 && contCuerpoPort && contHeader){
 
-// Evento del boton de Java
-btnJava.addEventListener("click", () => {
-    contJava.classList.toggle("active")
-    contC.classList.remove('active')
-    contHtml.classList.remove('active')
+        btnAparecer.addEventListener('mouseover', () =>{
+            svgFlecha.forEach(svg =>{
+                svg.style.fill = '#eaf2f9'
+                svg.style.transform = 'rotate(3.142rad)'
+                svg.style.transition = 'transform 1s linear'
+            })
+        })
+        btnAparecer.addEventListener('mouseout', () =>{
+            if(!btnPresionado){
+                svgFlecha.forEach(svg =>{
+                svg.style.transform = 'rotate(0deg)'
+            })
+            }
+        })
+        /*Este evento sirve para varias cosas por ejemplo para agregar al SVG mediante un foreach la clase
+        vuelta, para hacer rotar el svg, tambien para habilitar o desabilitar el mosueout,
+        agrega a el contenedor cuerpoHeader la clase ocultar y al mismo contenedor que funciona como 
+        footer lo hace pequeño para que al presionar funcione como cabecera. */
+        btnAparecer.addEventListener('click', () =>{
+            btnPresionado = true
+            svgFlecha.forEach(svg =>{
+                if(svg.classList.contains('vuelta')){
+                    svg.classList.remove('vuelta')
+                    btnPresionado = false
+                }else{
+                    svg.classList.toggle('vuelta')
+                }
+            })
+            contCuerpoPort.classList.toggle('ocultar')
+            if(contHeader.classList.contains('reducir')){
+                contHeader.classList.remove('reducir')
+                txtCambiar.innerText = 'Mis proyectos'
+            }else{
+                contHeader.classList.toggle('reducir')
+                let valor = txtCambiar.textContent
+                console.log(`${valor}`)
+                txtCambiar.innerText = 'Sobre mi'
+            }
+        })
+    }
 })
-
-// Evento de boton de c#
-
-btnC.addEventListener("click", () => {
-    contC.classList.toggle("active")
-    contJava.classList.remove("active")
-    contHtml.classList.remove("active")
-})
-
-// Evento de boton de html
-btnHtml.addEventListener("click", () => {
-    contHtml.classList.toggle("active")
-    contJava.classList.remove("active")
-    contC.classList.remove("active")
-})
-
-
-// Estos ultimos tres botones son para cambiar el color de las listas
-// para mostrar el color de cada habilidad
-
-// VARIABLES DE LOS BOTONES
-const btnBasi = document.getElementById('btnBasi')
-const btnInter = document.getElementById('btnInter')
-const btnEva = document.getElementById('btnEva')
-
-// VARIABLES DE LOS CONTENEDORES 
-const elementosInter = document.querySelectorAll('.opcInt'); 
-const elementoBasi = document.querySelectorAll('.opcBasi');
-
-// Funcionalida de los botones
-btnBasi.addEventListener('click', () => {
-    elementoBasi.forEach(elemento => { 
-        elemento.classList.toggle('active');
-    });
-});
-
-btnInter.addEventListener('click', () => {
-    elementosInter.forEach(elemento => { 
-        elemento.classList.toggle('active');
-    });
-});
-
-
-btnInter.addEventListener('click', () => {
-    contInter.classList.toggle("active")
-})
-
